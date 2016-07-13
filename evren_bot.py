a667fe1f4fb71b406c3179f2633daff6933fc060
Zach Dwiel <zach@nervanasys.com>
2016-07-13 10:49:04 -0700

    first commit

---
+++ evren_bot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import time
+import random
+import requests
+import os
+
+
+def build_message(phrase_sets):
+    message = ''
+    for phrase_set in phrase_sets:
+        message += random.choice(phrase_set)
+    return message
+
+PHRASE_SETS = [
+    ['', "dont forget to "],
+    ['order'],
+    ['', ' your'],
+    [' lunch', ' lunches'],
+    ['', ' people', ' everyone'],
+]
+
+
+def wait_random_time_between(minutes_min, minutes_max):
+    seconds_min = minutes_min * 60
+    seconds_max = minutes_max * 60
+    seconds = random.randrange(seconds_min, seconds_max)
+    print 'waiting', seconds, 'seconds'
+    time.sleep(seconds)
+
+
+def main():
+    wait_random_time_between(1, 9)
+
+    message = build_message(PHRASE_SETS)
+
+    print message
+
+    os.system("""curl -X POST --data-urlencode 'payload={{"channel": "#sd", "username": "evren-bot", "text": "{message}", "icon_emoji": ":whale2:"}}' {hook_url}""".format(
+        message=message,
+        hook_url=os.environ['SLACK_HOOK_URL'],
+    ))
+
+if __name__ == '__main__':
+    main()
